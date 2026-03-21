4aa24eef5197acb4a2ca832860b10491484a6941
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2026-03-21 22:08:22 +0100

    chore: make sure the commit id is different before updating

--- update.sh
+++ update.sh
@@ -3,6 +3,14 @@
 set -e
 
 echo "Actualizando el sistema..."
+old_rev="$(git rev-parse HEAD)"
 git pull --rebase --autostash
 
-nixos-rebuild switch --flake path:.#nixos
+new_rev="$(git rev-parse HEAD)"
+
+if [ "$old_rev" != "$new_rev" ]; then
+  echo "Hay cambios nuevos en el repositorio. Construyendo el sistema..."
+  nixos-rebuild switch --flake path:.#nixos
+else
+  echo "No hay cambios nuevos en el repositorio. Omitiendo nixos-rebuild."
+fi
