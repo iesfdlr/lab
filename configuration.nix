f44df7155afb24b10c9c0b635794759012f86f9b
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2026-03-22 15:31:56 +0100

    feat: firefox configuration

--- configuration.nix
+++ configuration.nix
@@ -202,9 +202,55 @@ in
     ]))
   ];
 
+  # firefox config, source: https://wiki.nixos.org/wiki/Firefox/en#Advanced
   programs.firefox = {
     enable = true;
     languagePacks = [ "es-ES" ];
+
+    policies = {
+      # Updates & Background Services
+      AppAutoUpdate                 = false;
+      BackgroundAppUpdate           = false;
+
+      # Feature Disabling
+      DisableFirefoxStudies         = true;
+      DisableFirefoxAccounts        = true;
+      DisableFirefoxScreenshots     = true;
+      DisableProfileImport          = true;
+      DisableProfileRefresh         = true;
+      DisableSetDesktopBackground   = true;
+      DisablePocket                 = true;
+      DisableTelemetry              = true;
+      DisableFormHistory            = true;
+
+      # Access Restrictions
+      BlockAboutConfig              = false;
+      BlockAboutProfiles            = true;
+      BlockAboutSupport             = true;
+
+	    # UI and Behavior
+	    DisplayMenuBar                = "never";
+	    DontCheckDefaultBrowser       = true;
+	    HardwareAcceleration          = false;
+	    OfferToSaveLogins             = false;
+      # force private browsing mode
+	    PrivateBrowsingModeAvailability = 2;
+	    DefaultDownloadDirectory      = "${home}/Downloads";
+
+      # Extensions
+      ExtensionSettings = let
+        moz = short: "https://addons.mozilla.org/firefox/downloads/latest/${short}/latest.xpi";
+      in {
+        "*".installation_mode = "blocked";
+
+        "uBlock0@raymondhill.net" = {
+          install_url       = moz "ublock-origin";
+          installation_mode = "force_installed";
+          updates_disabled  = true;
+          private_browsing  = true;
+        };
+      };
+    };
   };
 
   # sudo configuration
